bd3ce0ba7db6531a3384ce015dcdab00f45507ff
Akshaya Gosavi <akshayagosavi3@gmail.com>
2026-08-21 02:34:54 +0530

    Remove unused helper files

--- test_spark.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("test").master("local[*]").getOrCreate()
-print("PySpark version:", spark.version)
-spark.stop()
-print("SparkSession stopped cleanly.")
